33a79f1c4f9c4196713a8968211c5fb869488ac3
norrisLearnCode <92442398+norrisLearnCode@users.noreply.github.com>
2022-08-08 16:47:02 +0800

    Add files via upload

---
+++ java/Java_Snake_Game/src/com/company/Node.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.ArrayList;
+public class Node {
+    int x;
+    int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
