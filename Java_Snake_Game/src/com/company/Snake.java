8c5338498d000aade5634821ee64d8d7e098e88a
norrisLearnCode <92442398+norrisLearnCode@users.noreply.github.com>
2022-08-11 02:00:42 +0800

    Add files via upload

---
+++ Java_Snake_Game/src/com/company/Snake.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Snake {
+    private ArrayList<Node> snakeBody;
+
+    public Snake() {
+        snakeBody = new ArrayList<>();
+        snakeBody.add(new Node(40, 0));
+        snakeBody.add(new Node(20, 0));
+        snakeBody.add(new Node(0, 0));
+    }
+
+    public ArrayList<Node> getSnakeBody() {
+        return snakeBody;
+    }
+
+    public void drawSnake(Graphics g) {
+        for (int i = 0; i < snakeBody.size(); i++) {
+            if (i == 0) {
+                g.setColor(Color.green);
+            } else {
+                g.setColor(Color.yellow);
+            }
+        Node n = snakeBody.get(i);
+            if (n.x >= Main.width) {
+                n.x = 0;
+            }
+            if (n.x < 0) {
+                n.x = Main.width - Main.CELL_SIZE;
+            }
+            if (n.y >= Main.height) {
+                n.y = 0;
+            }
+            if (n.y < 0) {
+                n.y = Main.height - Main.CELL_SIZE;
+            }
+            g.fillOval(n.x, n.y, Main.CELL_SIZE, Main.CELL_SIZE);
+        }
+    }
+}
